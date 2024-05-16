88b4aeced04bad6e09c5fafeeff15e02c97cc0bc
amyel <oamieljay@gmail.com>
2024-05-15 19:50:13 -1000

    recaptcha removal

--- deploy/controllers/loginController.js
+++ deploy/controllers/loginController.js
@@ -5,7 +5,7 @@ const CryptoJS = require('crypto-js')
 const jwt = require('jsonwebtoken')
 const axios = require('axios')
 
-function handleLogin(req, res) {
+async function handleLogin(req, res) {
   const inputs = {
     "username": req.body.username,
     "password": req.body.password
@@ -21,15 +21,15 @@ function handleLogin(req, res) {
     data.append('secret', recaptchaSecretKey);
     data.append('response', inputs.recaptchaResponse);
 
-    axios.post(verificationUrl, data)
-      .then(async response => {
-        const body = response.data;
+    // axios.post(verificationUrl, data)
+    //   .then(async response => {
+    //     const body = response.data;
 
-        if (!body.success) {
-          // reCAPTCHA verification failed
-          console.log("RECAPTCHA failed!")
-          return res.status(403).json({ message: 'reCAPTCHA failed' });
-        } else {
+        // if (!body.success) {
+        //   // reCAPTCHA verification failed
+        //   console.log("RECAPTCHA failed!")
+        //   return res.status(403).json({ message: 'reCAPTCHA failed' });
+        // } else {
           // reCAPTCHA verification passed
           console.log("RECAPTCHA passed!")
           const user = await validateLoginInput(inputs);
@@ -53,8 +53,8 @@ function handleLogin(req, res) {
                 await insertRefreshToken(refreshToken, user.username)
                 return res.status(200).json({ user: user_info, access_token: accessToken, refresh_token: refreshToken });
               }
-        }
-      })
+        // }
+      // })
   } catch (error) {
     console.error("Error verifying reCAPTCHA:", error);
     return res.status(403).json({ message: 'Error verifying reCAPTCHA' });
